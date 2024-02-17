d8a859f24d7671f201145a3f47a5991e1bdac368
1stby <love970120@gmail.com>
2024-02-17 23:41:55 +0800

    add 登入驗證

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    username = StringField('使用者名', validators=[DataRequired()])
+    username = StringField('使用者名稱', validators=[DataRequired()])
     password = PasswordField('密碼', validators=[DataRequired()])
     remember_me = BooleanField('記住我')
     submit = SubmitField('登入')
