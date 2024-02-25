250095d9c3f30e6be9146aef915613ecd00e57b1
1stby <150894981+1stby@users.noreply.github.com>
2024-02-25 11:36:58 +0800

    Update config.py

--- microblog/config.py
+++ microblog/config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        "mysql+pymysql://root:lee970120@localhost:3306/data"
+        "mysql+pymysql://root:root@localhost:3306/data"
